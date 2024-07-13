9632aace5860f008a69425744d9b987f29d09db7
AditiJHA-ai <aditijha1104@gmail.com>
2024-07-13 21:39:57 +0530

    Update Chat.jsx

--- Chat.jsx
+++ Chat.jsx
@@ -1,7 +1,103 @@
-const Chat = () => {
-    return(
-        <div className='chat'>Chat</div>
-)
-}
 
-export default Chat
+import React, { useState, useRef, useEffect } from "react";
+import EmojiPicker from "emoji-picker-react";
+
+const Chat = () => {
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
+  const handleEmoji = (e, emojiObject) => {
+    setText((prev) => prev + emojiObject.emoji);
+    setOpen(false);
+  };
+
+  console.log(text);
+
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Aditi Jha</span>
+            <p>My friend</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center">
+        <img src="./avatar.png" alt="" />
+        <div className="message">
+          <div className="texts">
+            <p>hey how you doing</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>hey how you doing</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>hey how you doing</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>hey how you doing</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>hey how you doing</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="//copy and paste some image link//" alt="" />
+            <p>hey how you doing</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div ref={endRef}></div>
+      </div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./image.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img src="./emoji.png" alt="" onClick={() => setOpen((prev) => !prev)} />
+          {open && <EmojiPicker onEmojiClick={handleEmoji} />}
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
