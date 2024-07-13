63cf9cb63eb4abaf82dbec586bdd725e7305744b
AditiJHA-ai <aditijha1104@gmail.com>
2024-07-13 21:38:59 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,11 +1,38 @@
+import { onAuthStateChanged } from "firebase/auth";
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import List from "./components/list/List";
+import Notifications from "./components/notifications/notifications";
+
 const App = () => {
+
+const user = false;
+
+useEffect(()=>{
+  const unSub = onAuthStateChanged(auth,(user)=>{
+    console.log(user);
+  });
+
+  return () => {
+    unSub();
+  };
+},[]);
+
   return (
     <div className='container'>
-      <List/>
-      <Chat/>
-      <Detail/>
-      </div>
-  )
-}
+      {
+        user ? (
+        <>          
+          <List/>
+          <Chat/>
+          <Detail/>
+        </>
+      ) : (
+        <Login/>
+      )}
+      <Notifications/>
+    </div>
+  );
+};
 
 export default App
