70c7686d2775d9267b66b2c1246a2372d8fb8145
AditiJHA-ai <aditijha1104@gmail.com>
2024-07-15 19:58:46 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,18 +3,17 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "myntrachannel-ac2b3.firebaseapp.com",
-  projectId: "myntrachannel-ac2b3",
-  storageBucket: "myntrachannel-ac2b3.appspot.com",
-  messagingSenderId: "662758075080",
-  appId: "1:662758075080:web:a45eb9e2661e681e54d3fd",
-  measurementId: "G-MDQVFZ0Q29"
-};
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: "chats-868ba.firebaseapp.com",
+  projectId: "chats-868ba",
+  storageBucket: "chats-868ba.appspot.com",
+  messagingSenderId: "798443487103",
+  appId: "1:798443487103:web:7dd52953894b0b5117644a",
+  measurementId: "G-08SQDSCN1H"
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 export const auth = getAuth()
 export const db = getFirestore()
 export const storage = getStorage()
